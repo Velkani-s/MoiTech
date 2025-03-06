a9ceb3ca0fff7dede6b13fd400da02daf847d235
Batman-IronMan-007 <velkani.s@vjansoftware.com>
2025-03-06 17:33:36 +0530

    update

--- src/Components/Contact us/contactus.jsx
+++ src/Components/Contact us/contactus.jsx
@@ -1,30 +1,24 @@
 import React from "react";
 import Header from "../header/header";
 import Footer from "../footer/footer";
-import './contactus.css'
+import "./contactus.css";
 import Parallax from "parallax-js";
 
-
 const contactus = () => {
+  //   var scene = document.getElementById('scene');
+  //   var parallaxInstance = new Parallax (scene,
+  //   {
+  //     relativeInput:true,
 
-
-//   var scene = document.getElementById('scene');
-//   var parallaxInstance = new Parallax (scene,
-//   {
-//     relativeInput:true,
-    
-//   });
-// parallaxInstance.friction(0.2, 0.2);
+  //   });
   return (
     <>
       <Header />
       <section className="content">
-        <div className="text">
-          Contact ourselves
-        </div>
+        <div className="text">Contact ourselves</div>
       </section>
       {/* <Footer/> */}
-      </>
+    </>
   );
 };
 
