aa57b4ebb247506b6ced9888e93c24779294da4b
Batman-IronMan-007 <velkani.s@vjansoftware.com>
2025-03-07 10:38:59 +0530

    learn

--- src/Components/adminRegister/AdminRegister.jsx
+++ src/Components/adminRegister/AdminRegister.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
-import Header from '../header/header'
-import Footer from '../footer/footer'
+import React, { useState } from "react";
+import Header from "../header/header";
+import Footer from "../footer/footer";
 
 const AdminRegister = () => {
+  const [AdminUser, SetAdminUser] = useState({});
+
   return (
     <>
-    <Header/>
-     <div>AdminRegister</div>
-     <Footer/>
-     </>
-  
-  )
-}
+      <Header />
+      <section className="container">
+        <div className="row">
+          <div className="col-lg-6">
+            <h2>Earn with us Grow with us</h2>
+          </div>
+          <div className="col-lg-6">
+            
+          </div>
+        </div>
+      </section>
+      <Footer />
+    </>
+  );
+};
 
-export default AdminRegister
+export default AdminRegister;
